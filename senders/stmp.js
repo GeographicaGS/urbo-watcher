074e7af1d7e1e7c9ced855f5edd91f7ce1af3f2b
rbsolis <bob.solis@gmail.com>
2018-10-16 10:07:55 +0200

    Transform Sender into a Class.

--- senders/stmp.js
+++ senders/stmp.js
@@ -1,40 +1,78 @@
 'use strict';
 
-const nodemailer = require('nodemailer');
+var nodemailer = require('nodemailer');
+var config = require('../config.js');
+var utils = require('../utils');
+var log = utils.log();
 
-// Generate test SMTP service account from ethereal.email
-// Only needed if you don't have a real mail account for testing
-nodemailer.createTestAccount((err, account) => {
-    // create reusable transporter object using the default SMTP transport
-    let transporter = nodemailer.createTransport({
-        host: 'smtp.ethereal.email',
-        port: 587,
-        secure: false, // true for 465, false for other ports
-        auth: {
-            user: account.user, // generated ethereal user
-            pass: account.pass // generated ethereal password
-        }
+class EmailSender {
+
+  constructor() {
+    var ecfg = config.getData().email;   
+
+    this._transporter = new nodemailer.createTransport({
+      host: ecfg.server_address,
+      port: ecfg.secure ? ecfg.ssl_port : ecfg.ttl_port,
+      secure: ecfg.secure, // using SSL at port 465, false for other ports or TTL
+      auth: {
+        user: ecfg.user,
+        pass: ecfg.password
+      }
     });
+ 
+    this._receivers = ecfg.receivers;
 
-    // setup email data with unicode symbols
-    let mailOptions = {
-        from: '"Fred Foo 👻" <foo@example.com>', // sender address
-        to: 'bar@example.com, baz@example.com', // list of receivers
-        subject: 'Hello ✔', // Subject line
-        text: 'Hello world?', // plain text body
-        html: '<b>Hello world?</b>' // html body
-    };
+    this._notifs = config.getData().notifications;
 
+  }
+  
+
+  sendMail(msg) {
+      var receivers = this._receivers;
+      var notifPrefix = this._notifs.subj_prefix || 'Urbo-Watcher';
+  
+      var payload = {
+          default: `Urbo-Watcher report, ${msg.report}`,
+          email: `Urbo-Watcher report, ${msg.report}`
+      };
+  
+      var mailOptions = {
+        subject: `[${notifPrefix}] ${msg.subject}`,
+        html: JSON.stringify(payload),
+        text: JSON.stringify(payload),
+        from: notifPrefix,
+        to: receivers
+      };
+      
     // send mail with defined transport object
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            return console.log(error);
-        }
-        console.log('Message sent: %s', info.messageId);
-        // Preview only available when sending through an Ethereal account
-        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    this._transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+          return console.log(error);
+      }
 
-        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-        // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+      console.log('Message sent: %s', info.messageId);
+
+      // Preview only available when sending through an Ethereal account
+      console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+      // Example log: 
+      // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+      // Example: Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+
+  });
+  }
+
+  verifySending() {
+    // verify connection configuration
+    this._transporter.verify(function(error, success) {
+      if (error) {
+          console.log(error);
+      } else {
+          console.log('Server is ready to take our messages');
+      }
     });
-});
+  } 
+
+}
+  
+module.exports = EmailSender;
+  
